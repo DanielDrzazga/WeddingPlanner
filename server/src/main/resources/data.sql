cfe9dfa9385511a2ec4857ebcbf0597e3fda9a3f
Daniel Drzazga <whater.d2@gmail.com>
2020-03-21 17:51:08 +0100

    sample of database Guest class

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO guest VALUES (1, 'Jan', 'Kowalski');
+INSERT INTO guest VALUES (2, 'Jadwiga', 'Kowalska');
+INSERT INTO guest VALUES (3, 'Adam', 'Szulc');
+INSERT INTO guest VALUES (4, 'Ewa', 'Kwiatkowska');
+INSERT INTO guest VALUES (5, 'Janusz', 'Kwiatkowski');
