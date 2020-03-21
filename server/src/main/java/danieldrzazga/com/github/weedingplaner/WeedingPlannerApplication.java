67828a869deeba9f3c40c2aa47ae97bf2c53d9cb
Daniel Drzazga <whater.d2@gmail.com>
2020-03-21 16:03:59 +0100

    init

---
+++ server/src/main/java/danieldrzazga/com/github/weedingplaner/WeedingPlannerApplication.java
@@ -0,0 +1,13 @@
+package danieldrzazga.com.github.weedingplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedingPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeedingPlannerApplication.class, args);
+    }
+
+}
